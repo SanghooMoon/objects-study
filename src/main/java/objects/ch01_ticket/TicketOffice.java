a6ad086bf7df54d9e0b6b10556d4a161e3ea5c72
SanghooMoon <sanghoo.moon@gmail.com>
2021-12-12 21:53:07 +0900

    티켓을 판매/교환하는 매표소 구현

---
+++ src/main/java/objects/ch01_ticket/TicketOffice.java
@@ -0,0 +1,28 @@
+package objects.ch01_ticket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 티켓을 판매/교환하는 매표소
+public class TicketOffice {
+    private Long amount;
+    private List<Ticket> tickets = new ArrayList<>();
+
+    public TicketOffice(Long amount, Ticket... tickets) {
+        this.amount = amount;
+        this.tickets.addAll(Arrays.asList(tickets));
+    }
+
+    public Ticket getTicket() {
+        return tickets.remove(0);
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+}
