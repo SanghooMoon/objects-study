eea517e8491f2dcc327edbee25f143dcde0fadd2
SanghooMoon <sanghoo.moon@gmail.com>
2021-12-12 21:49:07 +0900

    관람객 구현

---
+++ src/main/java/objects/ch01_ticket/Audience.java
@@ -0,0 +1,14 @@
+package objects.ch01_ticket;
+
+// 관람객
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
