a44d3eef56bd2aef2eadabe088fabcbc2c224751
SanghooMoon <sanghoo.moon@gmail.com>
2021-12-12 22:49:56 +0900

    리팩토링 : 판매원을 자율적인 존재로 만들자
    - 소극장의 enter() 메서드의 구현코드를 판매원의 sellTo 메서드로 이동
    - 판매원의 getTicketOffice()를 제거하므로써 판매원의 매표소 인스턴스 변수에 대한 타 객체의 접근 차단(캡슐화)

--- src/main/java/objects/ch01_ticket/Theater.java
+++ src/main/java/objects/ch01_ticket/Theater.java
@@ -10,15 +10,6 @@ public class Theater {
 
     // 관람객을 맞이하는 메서드
     public void enter(Audience audience) {
-        // 초대장을 가진 관람객 : 초대장 -> 티켓 교환
-        if(audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else { // 초대장이 없는 관람객 : 현금 -> 티켓 구매
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
--- src/main/java/objects/ch01_ticket/TicketSeller.java
+++ src/main/java/objects/ch01_ticket/TicketSeller.java
@@ -8,7 +8,16 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        // 초대장을 가진 관람객 : 초대장 -> 티켓 교환
+        if(audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else { // 초대장이 없는 관람객 : 현금 -> 티켓 구매
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
