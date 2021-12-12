446a10bf9ab66b007240a6b50045b0e607213d73
SanghooMoon <sanghoo.moon@gmail.com>
2021-12-12 21:55:08 +0900

    티켓 판매원 구현

---
+++ src/main/java/objects/ch01_ticket/TicketSeller.java
@@ -0,0 +1,14 @@
+package objects.ch01_ticket;
+
+// 티켓을 교환해주거나 판매하는 역할인 판매원
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
