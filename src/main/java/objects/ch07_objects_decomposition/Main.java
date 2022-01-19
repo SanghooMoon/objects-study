55c2e234f949fb72e1702d803960df686f58766e
SanghooMoon <sanghoo.moon@gmail.com>
2022-01-19 22:44:53 +0900

    급여관리시스템 - 하향식 기능 분해 문제점 확인을 위한 구조 변경

--- src/main/java/objects/ch07_objects_decomposition/Main.java
+++ src/main/java/objects/ch07_objects_decomposition/Main.java
@@ -3,18 +3,14 @@ package objects.ch07_objects_decomposition;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
 
 // 급여관리시스템
 public class Main {
 
-    static final HashMap<String, Integer> employeeInfos = new HashMap() {
-        {
-            put("문상후", 500);
-            put("홍길동", 400);
-            put("가나다", 300);
-        }
-    };
+    static final List<String> names = Arrays.asList("문상후", "홍길동", "가나다");
+    static final List<Double> pays = Arrays.asList(500d, 400d, 300d);
 
     // 직원의 급여를 계산하자.
     public static void main(String[] args) throws IOException {
@@ -32,37 +28,37 @@ public class Main {
     }
 
     private static void sumOfBasePays() {
-        int result = 0;
-        for(int pay : employeeInfos.values()) {
+        double result = 0;
+        for(double pay : pays) {
             result += pay;
         }
-        System.out.printf("직원 총 급여 : %d", result);
+        System.out.printf("직원 총 급여 : %.0f", result);
 
     }
 
     private static void calculatePay(String name) throws IOException {
-        int taxRate = getTaxRate();
-        int pay = calculatePayFor(name, taxRate);
+        double taxRate = getTaxRate();
+        double pay = calculatePayFor(name, taxRate);
         describeResult(name, pay);
     }
 
-    private static int getTaxRate() throws IOException {
+    private static double getTaxRate() throws IOException {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.print("세율을 입력하세요 : ");
         String taxRate = br.readLine();
 
-        return Integer.parseInt(taxRate);
+        return Double.parseDouble(taxRate);
     }
 
-    private static int calculatePayFor(String name, int taxRate) {
-        int basePay = employeeInfos.get(name);
+    private static double calculatePayFor(String name, double taxRate) {
+        double basePay = pays.get(names.indexOf(name));
 
         return basePay - (basePay * taxRate);
     }
 
-    private static void describeResult(String name, int pay) {
-        System.out.printf("이름 : %s, 급여 : %d", name, pay);
+    private static void describeResult(String name, double pay) {
+        System.out.printf("이름 : %s, 급여 : %.0f", name, pay);
     }
 
 }
